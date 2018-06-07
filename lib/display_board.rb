bd68312dae5b6df3ff87518ddd8c6c4e0ad4e677
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-07 20:48:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-
-
-
+def display_board(board)
+  welcome = "Welcome to Tic Tac Toe"
+  box = "   "+"|"+"   "+"|"+"   "
+  line = "-----------"
+  puts box 
+  puts line 
+  puts box
+  puts line
+  puts box
+end
 
 board=[" "," "," "," "," "," "," "," "," "]
 def display_board(board)
